6db8c6d75758ce91454b3dbf7c208779ce985623
Ben McNees <mcneesbr@gmail.com>
2018-08-16 22:49:06 -0400

    Added basic while loop to accept user input and return simple integers based on 'function' provided with break on 'exit'.

--- InteractiveArithmetic/Program.fs
+++ InteractiveArithmetic/Program.fs
@@ -1,7 +1,31 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    let mutable inpt = Console.ReadLine ()
+
+    while inpt <> "exit" do
+        let res = match inpt with
+                  | "add" -> 1
+                  | "sub" -> 2
+                  | "mul" -> 3
+                  | "div" -> 4
+                  | "sq" -> 5
+                  | "sqrt" -> 6
+                  | "pow" -> 7
+                  | "abs" -> 8
+                  | "ceil" -> 9
+                  | "floor" -> 10
+                  | "sum" -> 11
+                  | "avg" -> 12
+                  | "med" -> 13
+                  | "mod" -> 14
+                  | "max" -> 15
+                  | "min" -> 16
+                  | _ -> -1
+                
+        printfn "%A" res
+
+        inpt <- Console.ReadLine ()
+
+    0
