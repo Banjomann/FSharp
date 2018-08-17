27d67d76a7d2dd5b8de672ca2ec1eff95f1e2eff
Ben McNees <mcneesbr@gmail.com>
2018-08-16 21:53:42 -0400

    Modified Program.fs to only print a string literal 'Hello, world.'.

--- HelloWorld/Program.fs
+++ HelloWorld/Program.fs
@@ -1,7 +1,7 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
+﻿open System
 
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
-    0 // return an integer exit code
+    printfn "%s" "Hello, world."
+    let s = Console.ReadLine ()
+    0
